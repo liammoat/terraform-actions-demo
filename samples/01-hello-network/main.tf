94afcff020dc42eb96f5175f33dfbb0dba68721e
Liam Moat <contact@liammoat.com>
2021-05-13 10:32:32 +0100

    feat(hello-network): adding default subnet

--- samples/01-hello-network/main.tf
+++ samples/01-hello-network/main.tf
@@ -45,3 +45,11 @@ resource "azurerm_virtual_network" "default" {
   address_space       = ["10.0.0.0/16"]
   tags                = local.tags
 }
+
+# Create a subnet with the virtual network
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
