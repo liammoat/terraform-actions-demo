e593b37827c714cfe793194264ebd1721648e0cc
Liam Moat <contact@liammoat.com>
2021-05-13 12:24:46 +0100

    feat(vm-deploy): update source image ref

--- samples/05-vm-deploy/main.tf
+++ samples/05-vm-deploy/main.tf
@@ -83,7 +83,7 @@ resource "azurerm_linux_virtual_machine" "default" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
