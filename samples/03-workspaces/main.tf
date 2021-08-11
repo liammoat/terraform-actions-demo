7079f5655f07cd5b2ca25448e1408e171c93fe5a
Liam Moat <contact@liammoat.com>
2021-08-11 13:55:52 +0100

    feat(nsg): new security group

--- samples/03-workspaces/main.tf
+++ samples/03-workspaces/main.tf
@@ -65,3 +65,23 @@ resource "azurerm_network_security_group" "default" {
 
   tags = local.tags
 }
+
+resource "azurerm_network_security_group" "default2" {
+  name                = "default2nsg"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = local.tags
+}
